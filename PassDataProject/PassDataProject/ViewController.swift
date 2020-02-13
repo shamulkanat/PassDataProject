04f1b9a2e4b74e6a8bfaad8a2c00cc335722de98
Natallia <tarasevich.natallia@gmail.com>
2020-02-13 10:31:08 +0300

    The method "touchesBegan" added.
    The method "touchesBegan" added.

--- PassDataProject/PassDataProject/ViewController.swift
+++ PassDataProject/PassDataProject/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBAction func sendPressed(button: UIButton) {
+        
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+           self.view.endEditing(true)
+
+       }
 
 }
 

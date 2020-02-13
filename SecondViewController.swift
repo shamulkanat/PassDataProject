1ae8fdf41f56f2e86522e3ad00233ede36671abc
Natallia <tarasevich.natallia@gmail.com>
2020-02-13 11:49:52 +0300

    Make a link between viewController and objects in 1 and 2 screen
    Make a link between viewController and objects in 1 and 2 screen, write code (link), add constranes for objects in second screen.

---
+++ SecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  PassDataProject
+//
+//  Created by Natallia Tarasevich on 2/13/20.
+//  Copyright © 2020 Natallia Tarasevich. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    @IBAction func sendPressed(button: UIButton) {
+        
+    }
+
+   
+}
